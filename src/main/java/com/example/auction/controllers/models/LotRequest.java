9571f4e83aff50e7bf68fd586a041819d02b5dcd
Timur <rahmatullintimur7777@gmail.com>
2022-04-08 21:32:14 +0300

    folder management, add LotEntity and LotRepository file

--- src/main/java/com/example/auction/controllers/models/LotRequest.java
+++ src/main/java/com/example/auction/controllers/models/LotRequest.java
@@ -1,4 +1,17 @@
 package com.example.auction.controllers.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LotRequest {
+    private String id;
+
+    private String name;
+    private String description;
+    private  Integer price;
 }
--- src/main/java/com/example/auction/services/LotService.java
+++ src/main/java/com/example/auction/services/LotService.java
@@ -1,4 +1,89 @@
 package com.example.auction.services;
 
+import com.example.auction.controllers.exceptions.LotNotExistException;
+import com.example.auction.controllers.models.LotDto;
+import com.example.auction.controllers.models.LotRequest;
+import com.example.auction.database.entities.LotEntity;
+import com.example.auction.database.repositories.LotRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class LotService {
+
+    private final LotRepository lotRepository;
+
+    public LotService(LotRepository lotRepository) {
+        this.lotRepository = lotRepository;
+    }
+
+    //////////////////////////////////////////////////////////////
+    //                    public
+    //////////////////////////////////////////////////////////////
+
+    public LotDto saveLot(LotRequest lotRequest) throws LotNotExistException {
+        if (lotRequest.getId() != null && lotRepository.existsById(lotRequest.getId())) {
+            throw new LotNotExistException();
+        }
+        LotEntity newLot = new LotEntity();
+        newLot.setName(lotRequest.getName());
+        newLot.setDescription(lotRequest.getDescription());
+        newLot.setPrice(lotRequest.getPrice());
+        lotRepository.save(newLot);
+
+        return new LotDto(
+                newLot.getId(),
+                newLot.getName(),
+                newLot.getDescription(),
+                newLot.getPrice()
+        );
+    }
+
+    public List<LotDto> getLots() {
+        List<LotEntity> allLots = lotRepository.findAll();
+
+        return allLots.stream()
+                .map(lotEntity -> new LotDto(
+                        lotEntity.getId(),
+                        lotEntity.getName(),
+                        lotEntity.getDescription(),
+                        lotEntity.getPrice()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    public LotDto getLot(String lotId) throws LotNotExistException {
+        Optional<LotEntity> existedStudent = lotRepository.findOptionalById(lotId);
+
+        LotEntity lot = existedStudent.orElseThrow(LotNotExistException::new);
+
+        return new LotDto(
+                lot.getId(),
+                lot.getName(),
+                lot.getDescription(),
+                lot.getPrice()
+        );
+    }
+
+    public void updateLot(LotRequest lotRequest) throws LotNotExistException {
+        if (lotRequest.getId() == null || !lotRepository.existsById(lotRequest.getId())) {
+            throw new LotNotExistException();
+        }
+
+        LotEntity lot = lotRepository.getById(lotRequest.getId());
+        lot.setName(lotRequest.getName());
+        lot.setDescription(lotRequest.getDescription());
+        lot.setPrice(lotRequest.getPrice());
+        lotRepository.save(lot);
+    }
+
+    public void deleteLot(String lotId) throws LotNotExistException {
+        Optional<LotEntity> existedLot = lotRepository.findOptionalById(lotId);
+
+        LotEntity lot = existedLot.orElseThrow(LotNotExistException::new);
+        lotRepository.delete(lot);
+    }
 }
