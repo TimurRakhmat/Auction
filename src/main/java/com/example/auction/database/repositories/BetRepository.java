ef0bdef0c9de007050f7864d94ce05da7f02b334
Никита Забелин <NAZabelin@mai.ru>
2022-04-11 14:52:40 +0300

    Get many bets

--- src/main/java/com/example/auction/database/repositories/BetRepository.java
+++ src/main/java/com/example/auction/database/repositories/BetRepository.java
@@ -1,15 +1,17 @@
 package com.example.auction.database.repositories;
 
 import com.example.auction.database.entities.Bet;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BetRepository extends JpaRepository<Bet, String>{
     Optional<Bet> findOptionalById(String id);
-
-    boolean existsById(String id);
+    List<Bet> findBetsByOwnerId(String id);
+    boolean existsById(@NonNull String id);
 }
--- src/main/java/com/example/auction/services/impl/BetServiceImpl.java
+++ src/main/java/com/example/auction/services/impl/BetServiceImpl.java
@@ -8,29 +8,33 @@ import com.example.auction.database.entities.Bet;
 import com.example.auction.database.repositories.BetRepository;
 import com.example.auction.security.models.OurAuthToken;
 import com.example.auction.services.BetService;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
 public class BetServiceImpl implements BetService {
     private final BetRepository betRepository;
-
-    public BetServiceImpl(BetRepository betRepository) {
+    private final ModelMapper modelMapper;
+    public BetServiceImpl(ModelMapper modelMapper, BetRepository betRepository) {
+        this.modelMapper = modelMapper;
         this.betRepository = betRepository;
     }
 
     @Override
-    public BetDto saveBet(BetRequest betRequest, OurAuthToken ourAuthToken) throws BetExistException {
+    public BetDto saveBet(BetRequest betRequest, OurAuthToken ourAuthToken) {
 
         Bet newBet = new Bet();
+
         newBet.setAmount(betRequest.getAmount());
 
         newBet.setLotId(betRequest.getLotId());
         newBet.setOwnerId(ourAuthToken.getUserId());
-
         betRepository.save(newBet);
 
         return new BetDto(newBet.getId(), newBet.getLotId(), newBet.getAmount(), newBet.getCreateDate());
@@ -50,7 +54,8 @@ public class BetServiceImpl implements BetService {
 
     @Override
     public List<BetDto> getUserBets(OurAuthToken ourAuthToken){
-        return null;
+        return betRepository.findBetsByOwnerId(ourAuthToken.getUserId()).stream()
+                .map(obj -> modelMapper.map(obj, BetDto.class)).collect(Collectors.toList());
     }
 
 }
